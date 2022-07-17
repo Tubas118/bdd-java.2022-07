9b9a41b0779e4c0c4a3ab82d0b9df39328530911
Tony D <adunc3@gmail.com>
2022-07-16 22:51:34 -0500

    AC-1.1 - BLUE: Code cleanup

--- src/test/java/com/example/bdd/java/controllers/PersonsControllerServiceTests.java
+++ src/test/java/com/example/bdd/java/controllers/PersonsControllerServiceTests.java
@@ -2,8 +2,6 @@ package com.example.bdd.java.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assumptions.assumeThat;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
@@ -11,11 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -25,7 +21,6 @@ import com.example.bdd.java.entities.PersonEntity;
 import com.example.bdd.java.models.Person;
 import com.example.bdd.java.models.PersonsCriteria;
 import com.example.bdd.java.repositories.PersonRepository;
-import com.example.bdd.java.utils.MappersForTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
