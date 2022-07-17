ec93826735b94075448432ccd312cf1ac55ecf90
Tony D <adunc3@gmail.com>
2022-07-16 21:56:58 -0500

    Initial project from Spring Initializr

---
+++ src/main/java/com/example/bdd/java/TddJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdd.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bdd/java/TddJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdd.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
