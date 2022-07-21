33594adb781aac9b2e883efab18d33500f0c1fd6
Tony D <adunc3@gmail.com>
2022-07-20 22:40:00 -0500

    BLUE - add test data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO person (id, firstname, lastname) VALUES ('mock-jdoe-1', 'Jane', 'Doe');
+INSERT INTO person (id, firstname, lastname) VALUES ('mock-jdoe-2', 'John', 'Doe');
+INSERT INTO person (id, firstname, lastname) VALUES ('mock-cken-1', 'Clark', 'Kent');
