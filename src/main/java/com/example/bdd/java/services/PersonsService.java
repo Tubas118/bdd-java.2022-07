b1e67584ec1fe539a7fbe7718c36f1a803c067d9
Tony D <adunc3@gmail.com>
2022-07-21 17:35:50 -0500

    AC-1.2 - GREEN: Retrieve fuzzy match persons

--- src/main/java/com/example/bdd/java/services/PersonsService.java
+++ src/main/java/com/example/bdd/java/services/PersonsService.java
@@ -1,11 +1,18 @@
 package com.example.bdd.java.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import com.example.bdd.java.entities.PersonEntity;
+import com.example.bdd.java.entities.PersonEntity.PersonEntityBuilder;
 import com.example.bdd.java.models.Person;
+import com.example.bdd.java.models.PersonsCriteria;
 import com.example.bdd.java.repositories.PersonRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -25,6 +32,30 @@ public class PersonsService {
 			return null;
 		}
 		return objectMapper.convertValue(foundPerson.get(), Person.class);
+	}
+	
+	public List<Person> findPersonsByCriteria(PersonsCriteria personsCriteria) {
+		if (personsCriteria == null) {
+			return null;
+		}
+		final PersonEntityBuilder entityCriteria = PersonEntity.builder();
+		
+		if (StringUtils.hasText(personsCriteria.getFirstname())) {		
+			entityCriteria.firstname(personsCriteria.getFirstname());
+		}
+		
+		if (StringUtils.hasText(personsCriteria.getLastname())) {		
+			entityCriteria.lastname(personsCriteria.getLastname());
+		}
+		
+		if (StringUtils.hasText(personsCriteria.getId())) {		
+			entityCriteria.id(personsCriteria.getId());
+		}
+		
+		List<PersonEntity> foundPersons = personRepository.findAll(Example.of(entityCriteria.build()));
+		return (CollectionUtils.isEmpty(foundPersons))
+				? null
+				: Arrays.asList(objectMapper.convertValue(foundPersons, Person[].class)); 
 	}
 	
 }
