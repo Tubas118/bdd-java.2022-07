3a56549fa84352afc8a1e272e9dfa1fe43a2cdd1
Tony D <adunc3@gmail.com>
2022-07-21 17:35:50 -0500

    AC-1.2 - GREEN: Retrieve fuzzy match persons

--- src/main/java/com/example/bdd/java/controllers/PersonsControllerService.java
+++ src/main/java/com/example/bdd/java/controllers/PersonsControllerService.java
@@ -1,11 +1,14 @@
 package com.example.bdd.java.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.bdd.java.services.PersonsService;
 import com.example.bdd.java.api.PersonsApiDelegate;
 import com.example.bdd.java.models.Person;
+import com.example.bdd.java.models.PersonsCriteria;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +25,15 @@ public class PersonsControllerService implements PersonsApiDelegate {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(person);
+	}
+
+	@Override
+	public ResponseEntity<List<Person>> searchPersons(PersonsCriteria personsCriteria) {
+		List<Person> persons = personsService.findPersonsByCriteria(personsCriteria);
+		if (persons == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(persons);
 	}
 
 }
