b26d6422393a0ca6364016e1d47f695b202993a4
Tony Duncan <Tubas118@users.noreply.github.com>
2026-04-28 21:56:13 -0500

    AC-1.1/1.2/1.3 - GREEN: PersonsService

--- src/main/java/com/example/bdd/java/services/PersonsService.java
+++ src/main/java/com/example/bdd/java/services/PersonsService.java
@@ -1,14 +1,22 @@
 package com.example.bdd.java.services;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
-import com.example.bdd.java.models.PersonsCriteria;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import com.example.bdd.java.entities.PersonEntity;
+import com.example.bdd.java.entities.PersonEntity.PersonEntityBuilder;
 import com.example.bdd.java.models.Person;
+import com.example.bdd.java.models.PersonsCriteria;
 import com.example.bdd.java.repositories.PersonRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -18,6 +26,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PersonsService {
 	
+	private static final ExampleMatcher findPersonsByCriteriaExample = ExampleMatcher.matchingAll()
+			.withMatcher("firstname", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+			.withMatcher("lastname", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+			.withMatcher("id", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+
 	private final PersonRepository personRepository;
 	
 	private final ObjectMapper objectMapper;
@@ -31,11 +44,34 @@ public class PersonsService {
 	}
 
 	public List<Person> findPersonsByCriteria(PersonsCriteria personsCriteria) {
-		return Collections.emptyList();
+		if (personsCriteria == null) {
+			return null;
+		}
+		final PersonEntityBuilder entityCriteria = PersonEntity.builder();
+
+		if (StringUtils.isNotBlank(personsCriteria.getFirstname())) {
+			entityCriteria.firstname(personsCriteria.getFirstname());
+		}
+
+		if (StringUtils.isNotBlank(personsCriteria.getLastname())) {
+			entityCriteria.lastname(personsCriteria.getLastname());
+		}
+
+		if (StringUtils.isNotBlank(personsCriteria.getId())) {
+			entityCriteria.id(personsCriteria.getId());
+		}
+
+		List<PersonEntity> foundPersons = personRepository.findAll(Example.of(entityCriteria.build(), findPersonsByCriteriaExample));
+		return (CollectionUtils.isEmpty(foundPersons))
+				? Collections.emptyList()
+				: Arrays.asList(objectMapper.convertValue(foundPersons, Person[].class));
 	}
 
 	public List<Person> findPersonByFuzzySearch(String wordSegment) {
-		return Collections.emptyList();
+		Set<PersonEntity> workingSet = new HashSet<>(personRepository.findByFirstnameContainingIgnoreCase(wordSegment));
+		workingSet.addAll(personRepository.findByLastnameContainingIgnoreCase(wordSegment));
+		workingSet.addAll(personRepository.findByIdContainingIgnoreCase(wordSegment));
+		return Arrays.asList(objectMapper.convertValue(workingSet, Person[].class));
 	}
 
 }
