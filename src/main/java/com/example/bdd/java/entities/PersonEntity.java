af65d55a201542965d946603286125efbbfd56f9
Tony Duncan <Tubas118@users.noreply.github.com>
2026-05-03 21:39:05 -0500

    Update for Java 17 and Spring Boot 3.5

--- src/main/java/com/example/bdd/java/entities/PersonEntity.java
+++ src/main/java/com/example/bdd/java/entities/PersonEntity.java
@@ -1,8 +1,8 @@
 package com.example.bdd.java.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
